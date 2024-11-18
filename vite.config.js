d0c11aa9dfa677620dd2a28c61fc5decc59e472d
dhanushbd03 <dhanushrocks03@gmail..com>
2024-11-18 23:20:12 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
-      target: "es2020",
+      target: 'es2020',
     },
   },
   esbuild: {
-    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
-    logOverride: { "this-is-undefined-in-esm": "silent" },
+    logOverride: {
+      'this-is-undefined-in-esm': 'silent',
+    },
   },
   plugins: [
     react({
       babel: {
-        plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
+        plugins: [
+          'babel-plugin-macros',
+          'babel-plugin-styled-components',
+        ],
       },
     }),
   ],
+  build: {
+    target: 'es2020',
+    outDir: 'dist',
+  },
 });
